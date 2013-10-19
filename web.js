4d2d0daa2cf02d6ba8f3375bc99eebbf5a10d380
blacki@gmail.com <bmigliozzi@NYMACBOOK2.local>
2013-10-19 00:45:13 -0400

    adding user route

--- web.js
+++ web.js
@@ -2,11 +2,21 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello NSA!');
+app.get('/', function(req, res) {
+  res.send('Hello NSA!');
+});
+
+app.get('/user', function(req, res) {
+	// console.log();
+
+	var name = req.query.name;
+  res.send('Hello '+name+'!');
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
